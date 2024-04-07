4584b783a72264f233fff26f3c89927e7acea035
so1ve <i@mk1.io>
2024-04-07 10:42:59 +0800

    test

--- plugins/jackett/__init__.py
+++ plugins/jackett/__init__.py
@@ -27,7 +27,7 @@ class Jackett(_PluginBase):
     # 主题色
     plugin_color = "#000000"
     # 插件版本
-    plugin_version = "0.0.3"
+    plugin_version = "0.0.4"
     # 插件作者
     plugin_author = "Ray"
     # 作者主页
@@ -71,6 +71,7 @@ class Jackett(_PluginBase):
 
             if self._run_once:
                 self._scheduler = BackgroundScheduler(timezone=settings.TZ)
+                print(self.get_indexers())
 
                 if self._cron:
                     logger.info(f"索引更新服务启动，周期：{self._cron}")
